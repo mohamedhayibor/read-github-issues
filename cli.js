70e2a5942db48ab74b2ed52a5dd4cd0904f9a8f0
Mohamed Hayibor <mohamedhayibor@gmail.com>
2016-08-16 12:01:41 -0400

    remove tabs > spaces

--- cli.js
+++ cli.js
@@ -25,10 +25,10 @@ nTabs = Number(nTabs);
 
 // check nTabs
 if (nTabs < 1 || nTabs > 25) {
-	console.log(`
-     Your number of tabs might be either negative or too big.
-	`);
-	process.exit(1);
+  console.log(`
+  Your number of tabs might be either negative or too big.
+  `);
+  process.exit(1);
 }
 
 // repoPath defines the root path to the repository
@@ -41,15 +41,15 @@ got(link)
     let $ = cheerio.load( res.body );
     let result = $('ul.Box-body.js-navigation-container.js-active-navigation-container').children();
 
-		/*
-		* The easiest way to get the job done:
-		*  1. Collect the `issue_id`s
-		*  2. strip the issue number
-		*/
+    /*
+    * The easiest way to get the job done:
+    *  1. Collect the `issue_id`s
+    *  2. strip the issue number
+    */
 
     if (result.length < 1) {
       console.log(`
-        You might want to use another query. This one is returning an empty result.
+   You might want to use another query. This one is returning an empty result.
       `);
     }
 
@@ -57,12 +57,12 @@ got(link)
       return result[idx].attribs;
     });
 
-		// Getting the ids then striping the first 6 characters `issue_`
+    // Getting the ids then striping the first 6 characters `issue_`
     let dataIds = attribs.map(tag => {
-			if (!!tag) {
-				return tag.id.slice(6);
-			}
-		}).filter( id => id !== undefined);
+      if (!!tag) {
+        return tag.id.slice(6);
+      }
+    }).filter( id => id !== undefined);
 
 
     // slice array to corresponding number of tabs then open up issue tabs
@@ -74,12 +74,12 @@ got(link)
 
   }).catch(err => {
     throw new Error(`
-	Something went wrong:
-	1. Make sure you are connected to the internet.
-	2. Make sure your number of tabs is greater than 0 and less than 26
-	3. Make sure your query points to the correct github path
+  Something went wrong:
+  1. Make sure you are connected to the internet.
+  2. Make sure your number of tabs is greater than 0 and less than 26
+  3. Make sure your query points to the correct github path
 
-	Error from sys:
-	* ${ err }
-		`);
+  Error from sys:
+  * ${ err }
+    `);
   })
